45a948c8fbb843246a5c8407492dc49f95f93c29
Chris Gonzales <chrisg32@gmail.com>
2017-10-18 21:43:00 -0500

    Creating common library

---
+++ Commons/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Commons.Extensions
+{
+    public static class EnumerableExtensions
+    {
+    }
+}
---
+++ CommonsTest/Extensions/EnumerableExtensionsTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CommonsTest.Extensions
+{
+    public class EnumerableExtensionsTest
+    {
+        [Fact]
+        public void TestCanary()
+        {
+            Assert.True(true);
+        }
+    }
+}
