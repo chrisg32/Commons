51ffd987187ae110a93fd0914e0b2a32a775e0e0
Chris Gonzales <chrisg32@gmail.com>
2017-10-18 23:31:41 -0500

    Added common library items from gists

---
+++ Commons/Collections/CompositeKeyMap.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Commons.Collections
+{
+    public class CompositeKeyMap<TKey, TValue> : BaseCompositeKeyMap<TKey, TValue>
+    {
+        public new TValue this[TKey key1]
+        {
+            get => base[key1];
+            set => base[key1] = value;
+        }
+
+        public new void Add(TKey key1, TValue value)
+        {
+            base.Add(key1, value);
+        }
+
+        public new bool ContainsKey(TKey key1)
+        {
+            return base.ContainsKey(key1);
+        }
+
+        public new bool Remove(TKey key1)
+        {
+            return base.Remove(key1);
+        }
+    }
+
+    public class CompositeKeyMap<TKey1, TKey2, TValue> : BaseCompositeKeyMap<Tuple<TKey1, TKey2>, TValue>
+    {
+        public TValue this[TKey1 key1, TKey2 key2]
+        {
+            get => this[Tuple.Create(key1, key2)];
+            set => this[Tuple.Create(key1, key2)] = value;
+        }
+
+        public void Add(TKey1 key1, TKey2 key2, TValue value)
+        {
+            Add(Tuple.Create(key1, key2), value);
+        }
+
+        public bool ContainsKey(TKey1 key1, TKey2 key2)
+        {
+            return ContainsKey(Tuple.Create(key1, key2));
+        }
+
+        public bool Remove(TKey1 key1, TKey2 key2)
+        {
+            return Remove(Tuple.Create(key1, key2));
+        }
+    }
+
+    public class CompositeKeyMap<TKey1, TKey2, TKey3, TValue> : BaseCompositeKeyMap<Tuple<TKey1, TKey2, TKey3>, TValue>
+    {
+        public TValue this[TKey1 key1, TKey2 key2, TKey3 key3]
+        {
+            get => this[Tuple.Create(key1, key2, key3)];
+            set => this[Tuple.Create(key1, key2, key3)] = value;
+        }
+
+        public void Add(TKey1 key1, TKey2 key2, TKey3 key3, TValue value)
+        {
+            Add(Tuple.Create(key1, key2, key3), value);
+        }
+
+        public bool ContainsKey(TKey1 key1, TKey2 key2, TKey3 key3)
+        {
+            return ContainsKey(Tuple.Create(key1, key2, key3));
+        }
+
+        public bool Remove(TKey1 key1, TKey2 key2, TKey3 key3)
+        {
+            return Remove(Tuple.Create(key1, key2, key3));
+        }
+    }
+
+    public class CompositeKeyMap<TKey1, TKey2, TKey3, TKey4, TValue> : BaseCompositeKeyMap<Tuple<TKey1, TKey2, TKey3, TKey4>, TValue>
+    {
+        public TValue this[TKey1 key1, TKey2 key2, TKey3 key3, TKey4 key4]
+        {
+            get => this[Tuple.Create(key1, key2, key3, key4)];
+            set => this[Tuple.Create(key1, key2, key3, key4)] = value;
+        }
+
+        public void Add(TKey1 key1, TKey2 key2, TKey3 key3, TKey4 key4, TValue value)
+        {
+            Add(Tuple.Create(key1, key2, key3, key4), value);
+        }
+
+        public bool ContainsKey(TKey1 key1, TKey2 key2, TKey3 key3, TKey4 key4)
+        {
+            return ContainsKey(Tuple.Create(key1, key2, key3, key4));
+        }
+
+        public bool Remove(TKey1 key1, TKey2 key2, TKey3 key3, TKey4 key4)
+        {
+            return Remove(Tuple.Create(key1, key2, key3, key4));
+        }
+    }
+
+    public class CompositeKeyMap<TKey1, TKey2, TKey3, TKey4, TKey5, TValue> : BaseCompositeKeyMap<Tuple<TKey1, TKey2, TKey3, TKey4, TKey5>, TValue>
+    {
+        public TValue this[TKey1 key1, TKey2 key2, TKey3 key3, TKey4 key4, TKey5 key5]
+        {
+            get => this[Tuple.Create(key1, key2, key3, key4, key5)];
+            set => this[Tuple.Create(key1, key2, key3, key4, key5)] = value;
+        }
+
+        public void Add(TKey1 key1, TKey2 key2, TKey3 key3, TKey4 key4, TKey5 key5, TValue value)
+        {
+            Add(Tuple.Create(key1, key2, key3, key4, key5), value);
+        }
+
+        public bool ContainsKey(TKey1 key1, TKey2 key2, TKey3 key3, TKey4 key4, TKey5 key5)
+        {
+            return ContainsKey(Tuple.Create(key1, key2, key3, key4, key5));
+        }
+
+        public bool Remove(TKey1 key1, TKey2 key2, TKey3 key3, TKey4 key4, TKey5 key5)
+        {
+            return Remove(Tuple.Create(key1, key2, key3, key4, key5));
+        }
+    }
+
+    public class CompositeKeyMap<TKey1, TKey2, TKey3, TKey4, TKey5, TKey6, TValue> : BaseCompositeKeyMap<Tuple<TKey1, TKey2, TKey3, TKey4, TKey5, TKey6>, TValue>
+    {
+        public TValue this[TKey1 key1, TKey2 key2, TKey3 key3, TKey4 key4, TKey5 key5, TKey6 key6]
+        {
+            get => this[Tuple.Create(key1, key2, key3, key4, key5, key6)];
+            set => this[Tuple.Create(key1, key2, key3, key4, key5, key6)] = value;
+        }
+
+        public void Add(TKey1 key1, TKey2 key2, TKey3 key3, TKey4 key4, TKey5 key5, TKey6 key6, TValue value)
+        {
+            Add(Tuple.Create(key1, key2, key3, key4, key5, key6), value);
+        }
+
+        public bool ContainsKey(TKey1 key1, TKey2 key2, TKey3 key3, TKey4 key4, TKey5 key5, TKey6 key6)
+        {
+            return ContainsKey(Tuple.Create(key1, key2, key3, key4, key5, key6));
+        }
+
+        public bool Remove(TKey1 key1, TKey2 key2, TKey3 key3, TKey4 key4, TKey5 key5, TKey6 key6)
+        {
+            return Remove(Tuple.Create(key1, key2, key3, key4, key5, key6));
+        }
+    }
+
+    public class CompositeKeyMap<TKey1, TKey2, TKey3, TKey4, TKey5, TKey6, TKey7, TValue> : BaseCompositeKeyMap<Tuple<TKey1, TKey2, TKey3, TKey4, TKey5, TKey6, TKey7>, TValue>
+    {
+        public TValue this[TKey1 key1, TKey2 key2, TKey3 key3, TKey4 key4, TKey5 key5, TKey6 key6, TKey7 key7]
+        {
+            get => this[Tuple.Create(key1, key2, key3, key4, key5, key6, key7)];
+            set => this[Tuple.Create(key1, key2, key3, key4, key5, key6, key7)] = value;
+        }
+
+        public void Add(TKey1 key1, TKey2 key2, TKey3 key3, TKey4 key4, TKey5 key5, TKey6 key6, TKey7 key7, TValue value)
+        {
+            Add(Tuple.Create(key1, key2, key3, key4, key5, key6, key7), value);
+        }
+
+        public bool ContainsKey(TKey1 key1, TKey2 key2, TKey3 key3, TKey4 key4, TKey5 key5, TKey6 key6, TKey7 key7)
+        {
+            return ContainsKey(Tuple.Create(key1, key2, key3, key4, key5, key6, key7));
+        }
+
+        public bool Remove(TKey1 key1, TKey2 key2, TKey3 key3, TKey4 key4, TKey5 key5, TKey6 key6, TKey7 key7)
+        {
+            return Remove(Tuple.Create(key1, key2, key3, key4, key5, key6, key7));
+        }
+    }
+
+    [DebuggerDisplay("Count = {" + nameof(Count) + "}")]
+    [System.Runtime.InteropServices.ComVisible(false)]
+    public abstract class BaseCompositeKeyMap<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>>
+    {
+        private readonly Dictionary<TKey, TValue> _map = new Dictionary<TKey, TValue>();
+
+        protected TValue this[TKey key]
+        {
+            get => _map[key];
+            set => _map[key] = value;
+        }
+
+        protected void Add(TKey key, TValue value)
+        {
+            _map.Add(key, value);
+        }
+
+        public void Clear()
+        {
+            _map.Clear();
+        }
+
+        protected bool ContainsKey(TKey key)
+        {
+            return _map.ContainsKey(key);
+        }
+
+        public bool ContainsValue(TValue value)
+        {
+            return _map.ContainsValue(value);
+        }
+
+        protected bool Remove(TKey key)
+        {
+            return _map.Remove(key);
+        }
+
+        public int Count => _map.Count;
+
+        public IEnumerator GetEnumerator()
+        {
+            return _map.GetEnumerator();
+        }
+
+        IEnumerator<KeyValuePair<TKey, TValue>> IEnumerable<KeyValuePair<TKey, TValue>>.GetEnumerator()
+        {
+            return _map.GetEnumerator();
+        }
+    }
+}
---
+++ Commons/Util/NaturalComparer.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Commons.Util
+{
+    public class NaturalComparer : IComparer<string>
+    {
+        private readonly bool _ignoreCase;
+        private readonly bool _ignoreWhitespace;
+
+        [Flags]
+        public enum NaturalComparerOptions
+        {
+            None,
+            IgnoreCase,
+            IgnoreWhiteSpace
+        }
+
+        public NaturalComparer(NaturalComparerOptions options = NaturalComparerOptions.None)
+        {
+            _ignoreCase = options.HasFlag(NaturalComparerOptions.IgnoreCase);
+            _ignoreWhitespace = options.HasFlag(NaturalComparerOptions.IgnoreWhiteSpace);
+        }
+
+        //less than zero = x is less than y
+        //zero = x equals y
+        //greater than zero = x is greater than y
+
+        /// <summary>
+        /// Compares two strings using a natural sort. Nulls and empty strings are treated as equal.
+        /// </summary>
+        /// <param name="left">The left string to compare.</param>
+        /// <param name="right">The right string to compare.</param>
+        /// <returns>If the right string is less than the left the return value will be less than zero.
+        ///  If the left string is greater than the right than a value greater than zero is returned.
+        ///  If the left string is equal to the right string zero is returned.</returns>
+        public int Compare(string left, string right)
+        {
+            //treat null and empty strings the same, also ignore leading and trailing whitespace
+            var x = left?.Trim() ?? string.Empty;
+            var y = right?.Trim() ?? string.Empty;
+
+            if (_ignoreCase)
+            {
+                x = x.ToLower();
+                y = y.ToLower();
+            }
+
+            if (_ignoreWhitespace)
+            {
+                var regex = new Regex(@"\s");
+                x = regex.Replace(x, string.Empty);
+                y = regex.Replace(y, string.Empty);
+            }
+
+            //we can't use iterator since we need to see the next value
+            var xarray = x.ToCharArray();
+            var yarray = y.ToCharArray();
+            int xindex = -1, yindex = -1;
+            while (true)
+            {
+                var xhas = xindex + 1 < xarray.Length;
+                var yhas = yindex + 1 < yarray.Length;
+
+                //neither string has another to compare so they are equal
+                if (!(xhas || yhas)) return 0;
+
+                //if one string is shorter than the other than the shorter comes first
+                if (!xhas) return -1;
+                if (!yhas) return 1;
+
+                xindex++;
+                yindex++;
+
+                var xchar = xarray[xindex];
+                var ychar = yarray[yindex];
+
+                var xIsNum = char.IsDigit(xchar);
+                var yIsNum = char.IsDigit(ychar);
+
+                //if both characters are numeric then we have to compare the full numeric string part
+                if (xIsNum && yIsNum)
+                {
+                    var xints = GetNumericString(xarray, ref xindex);
+                    var yints = GetNumericString(yarray, ref yindex);
+                    var xint = int.Parse(xints);
+                    var yint = int.Parse(yints);
+                    var iresult = xint.CompareTo(yint);
+                    if (iresult != 0) return iresult;
+                    continue;
+                }
+
+                //if one char is numeric but the other is not then the numeric comes first
+                if (xIsNum) return -1;
+                if (yIsNum) return 1;
+
+                //both characters are not numeric so we simply compare
+                var cresult = xchar.CompareTo(ychar);
+                if (cresult != 0) return cresult;
+            }
+
+        }
+
+        private string GetNumericString(IReadOnlyList<char> source, ref int index)
+        {
+            var sb = new StringBuilder();
+            while (index < source.Count && char.IsDigit(source[index]))
+            {
+                sb.Append(source[index]);
+                index++;
+            }
+            index--;
+            return sb.ToString();
+        }
+
+    }
+}
