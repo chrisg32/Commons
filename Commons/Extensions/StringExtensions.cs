c245355c1060bb6a32dc72f56ec7b5aa19037328
Chris Gonzales <chrisg32@gmail.com>
2017-10-27 03:27:06 -0500

    Fixed package references for test project, updates packages, added GenericParser

---
+++ Commons/Extensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Commons.Util;
+
+namespace Commons.Extensions
+{
+    public static class StringExtensions
+    {
+        public static T Parse<T>(string stringValue)
+        {
+            return GenericParser.Parse<T>(stringValue);
+        }
+    }
+}
---
+++ CommonsTest/Util/GenericParserTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CommonsTest.Util
+{
+    public class GenericParserTest
+    {
+        [Fact]
+        public void TestCanary()
+        {
+            
+        }
+    }
+}
