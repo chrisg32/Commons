0903262067f16f03742b29b2a0aefb8423f2a5a8
Chris Gonzales <chrisg32@gmail.com>
2017-11-14 23:36:16 -0600

    Fix for SafeDictionary

--- Commons/Collections/SafeDictionary.cs
+++ Commons/Collections/SafeDictionary.cs
@@ -5,74 +5,95 @@ using System.Collections.Generic;
 namespace CG.Commons.Collections
 {
     /// <summary>
-    /// A simple extension of Dictionary that does not throw any exception when a key is not found.
+    /// A simple IDictionary implementation that does not throw any exception when a key is not found.
     /// </summary>
     /// <typeparam name="TKey"></typeparam>
     /// <typeparam name="TValue"></typeparam>
-    public class SafeDictionary<TKey, TValue> : Dictionary<TKey, TValue>
+    public class SafeDictionary<TKey, TValue> : IDictionary<TKey, TValue>
     {
+        private readonly Dictionary<TKey, TValue> _data = new Dictionary<TKey, TValue>();
+
         public TValue DefaultValue { get; }
 
-        /// <summary>Initializes a new instance of the <see cref="T:System.Collections.Generic.Dictionary`2"></see> class that is empty, has the default initial capacity, and uses the default equality comparer for the key type.</summary>
         /// <param name="defaultValue">The default value that will be returned when a key is not found.</param>
         public SafeDictionary(TValue defaultValue = default(TValue))
         {
             DefaultValue = defaultValue;
         }
 
-        /// <summary>Initializes a new instance of the <see cref="T:System.Collections.Generic.Dictionary`2"></see> class that contains elements copied from the specified <see cref="T:System.Collections.Generic.IDictionary`2"></see> and uses the default equality comparer for the key type.</summary>
-        /// <param name="dictionary">The <see cref="T:System.Collections.Generic.IDictionary`2"></see> whose elements are copied to the new <see cref="T:System.Collections.Generic.Dictionary`2"></see>.</param>
-        /// <param name="defaultValue">The default value that will be returned when a key is not found.</param>
-        /// <exception cref="T:System.ArgumentNullException"><paramref name="dictionary">dictionary</paramref> is null.</exception>
-        /// <exception cref="T:System.ArgumentException"><paramref name="dictionary">dictionary</paramref> contains one or more duplicate keys.</exception>
-        public SafeDictionary(IDictionary<TKey, TValue> dictionary, TValue defaultValue = default(TValue)) : base(dictionary)
+        public TValue this[TKey key]
         {
-            DefaultValue = defaultValue;
+            get => ContainsKey(key) ? _data[key] : DefaultValue;
+            set => _data[key] = value;
         }
 
-        /// <summary>Initializes a new instance of the <see cref="T:System.Collections.Generic.Dictionary`2"></see> class that contains elements copied from the specified <see cref="T:System.Collections.Generic.IDictionary`2"></see> and uses the specified <see cref="T:System.Collections.Generic.IEqualityComparer`1"></see>.</summary>
-        /// <param name="dictionary">The <see cref="T:System.Collections.Generic.IDictionary`2"></see> whose elements are copied to the new <see cref="T:System.Collections.Generic.Dictionary`2"></see>.</param>
-        /// <param name="comparer">The <see cref="T:System.Collections.Generic.IEqualityComparer`1"></see> implementation to use when comparing keys, or null to use the default <see cref="T:System.Collections.Generic.EqualityComparer`1"></see> for the type of the key.</param>
-        /// <param name="defaultValue">The default value that will be returned when a key is not found.</param>
-        /// <exception cref="T:System.ArgumentNullException"><paramref name="dictionary">dictionary</paramref> is null.</exception>
-        /// <exception cref="T:System.ArgumentException"><paramref name="dictionary">dictionary</paramref> contains one or more duplicate keys.</exception>
-        public SafeDictionary(IDictionary<TKey, TValue> dictionary, IEqualityComparer<TKey> comparer, TValue defaultValue = default(TValue)) : base(dictionary, comparer)
+        #region Implementation
+
+        public void Add(TKey key, TValue value)
         {
-            DefaultValue = defaultValue;
+            _data.Add(key, value);
         }
 
-        /// <summary>Initializes a new instance of the <see cref="T:System.Collections.Generic.Dictionary`2"></see> class that is empty, has the default initial capacity, and uses the specified <see cref="T:System.Collections.Generic.IEqualityComparer`1"></see>.</summary>
-        /// <param name="comparer">The <see cref="T:System.Collections.Generic.IEqualityComparer`1"></see> implementation to use when comparing keys, or null to use the default <see cref="T:System.Collections.Generic.EqualityComparer`1"></see> for the type of the key.</param>
-        /// <param name="defaultValue">The default value that will be returned when a key is not found.</param> 
-        public SafeDictionary(IEqualityComparer<TKey> comparer, TValue defaultValue = default(TValue)) : base(comparer)
+        public bool ContainsKey(TKey key)
         {
-            DefaultValue = defaultValue;
+            return _data.ContainsKey(key);
         }
 
-        /// <summary>Initializes a new instance of the <see cref="T:System.Collections.Generic.Dictionary`2"></see> class that is empty, has the specified initial capacity, and uses the default equality comparer for the key type.</summary>
-        /// <param name="capacity">The initial number of elements that the <see cref="T:System.Collections.Generic.Dictionary`2"></see> can contain.</param>
-        /// <param name="defaultValue">The default value that will be returned when a key is not found.</param>
-        /// <exception cref="T:System.ArgumentOutOfRangeException"><paramref name="capacity">capacity</paramref> is less than 0.</exception>
-        public SafeDictionary(int capacity, TValue defaultValue = default(TValue)) : base(capacity)
+        public bool Remove(TKey key)
         {
-            DefaultValue = defaultValue;
+            return _data.Remove(key);
         }
 
-        /// <summary>Initializes a new instance of the <see cref="T:System.Collections.Generic.Dictionary`2"></see> class that is empty, has the specified initial capacity, and uses the specified <see cref="T:System.Collections.Generic.IEqualityComparer`1"></see>.</summary>
-        /// <param name="capacity">The initial number of elements that the <see cref="T:System.Collections.Generic.Dictionary`2"></see> can contain.</param>
-        /// <param name="comparer">The <see cref="T:System.Collections.Generic.IEqualityComparer`1"></see> implementation to use when comparing keys, or null to use the default <see cref="T:System.Collections.Generic.EqualityComparer`1"></see> for the type of the key.</param>
-        /// <param name="defaultValue">The default value that will be returned when a key is not found.</param>
-        /// <exception cref="T:System.ArgumentOutOfRangeException"><paramref name="capacity">capacity</paramref> is less than 0.</exception>
-        public SafeDictionary(int capacity, IEqualityComparer<TKey> comparer, TValue defaultValue = default(TValue)) : base(capacity, comparer)
+        public bool TryGetValue(TKey key, out TValue value)
         {
-            DefaultValue = defaultValue;
+            return _data.TryGetValue(key, out value);
         }
 
-        public new TValue this[TKey key]
+        public ICollection<TKey> Keys => _data.Keys;
+        public ICollection<TValue> Values => _data.Values;
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
         {
-            get => ContainsKey(key) ? base[key] : DefaultValue;
-            set => base[key] = value;
+            return _data.GetEnumerator();
         }
 
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Clear()
+        {
+            _data.Clear();
+        }
+
+        #region KeyValuePair Methods
+
+        public void Add(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        public int Count => _data.Count;
+        public bool IsReadOnly => false;
+
+        #endregion
     }
 }
--- CommonsTest/Collections/SafeDictionaryTest.cs
+++ CommonsTest/Collections/SafeDictionaryTest.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using CG.Commons.Collections;
 using Xunit;
 
 namespace CG.Commons.Test.Collections
@@ -13,6 +10,17 @@ namespace CG.Commons.Test.Collections
         public void TestCanary()
         {
             Assert.True(true);
+        }
+
+        /// <summary>
+        /// The old implmentation did not work if casting to the interface. We need to test for that.
+        /// </summary>
+        [Fact]
+        public void TestPolymorphism()
+        {
+            SafeDictionary<string, int> dict = new SafeDictionary<string, int>(7);
+            Assert.Equal(7, dict["foo"]);
+            Assert.Equal(7, ((IDictionary<string, int>)dict)["foo"]);
         }
     }
 }
