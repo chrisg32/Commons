dfd70e7e034c752e886ed7f6a3329212dbf995aa
Chris Gonzales <chrisg32@gmail.com>
2017-11-14 23:13:07 -0600

    Added SafeDictionary and SafeDictionary tests, updated the CommonsTest.csproj to work correctly as a submodule.

---
+++ CommonsTest/Collections/SafeDictionaryTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CG.Commons.Test.Collections
+{
+    public class SafeDictionaryTest
+    {
+        [Fact]
+        public void TestCanary()
+        {
+            Assert.True(true);
+        }
+    }
+}
