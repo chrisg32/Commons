430bd2f85b54fd84994264b9f20e4b900f7142c1
Chris Gonzales <chrisg32@gmail.com>
2018-02-14 22:52:33 -0600

    Added ObjectExtensions

---
+++ Commons/Extensions/ObjectExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CG.Commons.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static object GetPropertyValue(this object obj, string property)
+        {
+            try
+            {
+                var propertyTree = new Queue<string>(property.Split('.'));
+                while (propertyTree.Any())
+                {
+                    if (obj == null) return null;
+                    var propName = propertyTree.Dequeue();
+                    obj = obj.GetType().GetProperty(propName)?.GetValue(obj);
+                }
+                return obj;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
