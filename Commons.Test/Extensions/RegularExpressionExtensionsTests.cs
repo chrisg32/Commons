b8a41e45fe5804d6c88a240c99810cf03cd36945
Chris Gonzales <chrisg32@users.noreply.github.com>
2024-03-28 07:17:33 -0500

    Added Regex.ReplaceGroups extension

---
+++ Commons.Test/Extensions/RegularExpressionExtensionsTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+using CG.Commons.Extensions;
+using FluentAssertions;
+using Xunit;
+
+namespace CG.Commons.Test.Extensions;
+
+public class RegularExpressionExtensionsTests
+{
+    [Fact]
+    public void TestReplaceGroups_Array()
+    {
+        // Arrange
+        var s = "Hello Jon, please give me 72 tacos!";
+        var regex = new Regex(@"Hello (.+), please give me (\d+) tacos!");
+        
+        // Act
+        var result = regex.ReplaceGroups(s, "John", "42");
+        
+        // Assert
+        result.Should().Be("Hello John, please give me 42 tacos!");
+    }
+    
+    [Fact]
+    public void TestReplaceGroups_Func()
+    {
+        // Arrange
+        var s = "Hello Jon, please give me 72 tacos!";
+        var regex = new Regex(@"Hello (.+), please give me (\d+) tacos!");
+        
+        // Act
+        var result = regex.ReplaceGroups(s, i => $"{{{i}}}");
+        
+        // Assert
+        result.Should().Be("Hello {0}, please give me {1} tacos!");
+    }
+}
---
+++ Commons/Extensions/RegularExpressionExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CG.Commons.Extensions;
+
+public static class RegularExpressionExtensions
+{
+    /// <summary>
+    /// Replaces the regex capture groups in the input string with the specified replacements.
+    /// </summary>
+    /// <param name="regex">The regular expression.</param>
+    /// <param name="input">The input string.</param>
+    /// <param name="replacements">The replacements strings. Must have equal or greater number of values than groups.</param>
+    /// <returns>The match string with the capture groups replaced with the corresponding replacement.</returns>
+    public static string ReplaceGroups(this Regex regex, string input, params string[] replacements)
+    {
+        return regex.ReplaceGroups(input, index => replacements[index]);
+    }
+
+    /// <summary>
+    /// Replaces the regex capture groups in the input string with the value returned by the specified replacement function.
+    /// </summary>
+    /// <param name="regex">The regular expression.</param>
+    /// <param name="input">The input string.</param>
+    /// <param name="replacement">A function that given a zero based index of the group being replaced, returns a string that will replace the capture group.</param>
+    /// <returns>The match string with the capture groups replaced with the corresponding replacement.</returns>
+    public static string ReplaceGroups(this Regex regex, string input, Func<int, string> replacement)
+    {
+        return regex.Replace(input, match =>
+        {
+            var index = 0;
+            var sb = new StringBuilder();
+            for(var g = 1; g < match.Groups.Count; g++)
+            {
+                sb.Append(match.Value[index..match.Groups[g].Index]);
+                sb.Append(replacement(g - 1));
+                index = match.Groups[g].Index + match.Groups[g].Length;
+            }
+            if(index < match.Length)
+            {
+                sb.Append(match.Value[index..]);
+            }
+            return sb.ToString();
+        });
+    }
+}
