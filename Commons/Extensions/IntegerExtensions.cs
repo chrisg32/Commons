1f976188766eded6f9a66dbb6a5cad6aba23673b
Chris Gonzales <chrisg32@users.noreply.github.com>
2024-03-28 07:17:12 -0500

    Fixed: Int Extensions Times not executing if result is not iterated over.

--- Commons/Extensions/IntegerExtensions.cs
+++ Commons/Extensions/IntegerExtensions.cs
@@ -22,20 +22,24 @@ namespace CG.Commons.Extensions
             }
         }
 
-        public static IEnumerable<TResult> Times<TResult>(this int count, Func<TResult> function)
+        public static List<TResult> Times<TResult>(this int count, Func<TResult> function)
         {
+            var result = new List<TResult>();
             for (var i = 0; i < count; i++)
             {
-                yield return function();
+                result.Add(function());
             }
+            return result;
         }
 
-        public static IEnumerable<TResult> Times<TResult>(this int count, Func<int, TResult> function)
+        public static List<TResult> Times<TResult>(this int count, Func<int, TResult> function)
         {
+            var result = new List<TResult>();
             for (var i = 0; i < count; i++)
             {
-                yield return function(i);
+                result.Add(function(i));
             }
+            return result;
         }
 
         public static IEnumerable<int> Iterations(this int count)
